c802f6b2f7cfcd09fe820a24cd8ff86b2c902d39
gilbertoferrari <gilbertoferrari21@gmail.com>
2025-08-24 18:13:49 -0300

    criado repositorio remoto no Github

---
+++ src/main/java/mypackage/Santander2025Application.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/mypackage/Santander2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
